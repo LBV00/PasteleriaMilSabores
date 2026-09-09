83d5bb3eb2c79de3faaecc89bb23bc5884d548c8
valeitt <va.osses@duocuc.cl>
2026-09-08 23:46:19 -0300

    agregado contenido

---
+++ assets/js/menu.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function configurarMenuAccesible() {
+    document.querySelectorAll(".navbar-toggler").forEach(boton => {
+        const objetivoId = boton.getAttribute("data-bs-target");
+        if (!objetivoId) return;
+        const objetivo = document.querySelector(objetivoId);
+        if (!objetivo) return;
+
+        objetivo.addEventListener("shown.bs.collapse", () => boton.setAttribute("aria-expanded", "true"));
+        objetivo.addEventListener("hidden.bs.collapse", () => boton.setAttribute("aria-expanded", "false"));
+    });
+}
